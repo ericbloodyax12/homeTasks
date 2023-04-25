9949a769cc470823fa59b667acdde16c654ce4fa
Young Eric <erikbonopart12@gmail.com>
2023-04-25 21:58:31 +0300

    refactor: -refactor date state and some JSX,  -add property for date (toLocaleDateString) to stringMonth,stringDay

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -4,7 +4,7 @@ import {restoreState} from '../hw06/localStorage/localStorage'
 import s from './Clock.module.css'
 
 
-let msInSec = (sec: number) => 1000 * sec
+let msToSec = (sec: number) => 1000 * sec
 let timeCreator = (currentDate: Date) => {
     let time = [
         currentDate.getHours(),
@@ -20,7 +20,7 @@ let dateCreator = (currentDate: Date) => {
         currentDate.getFullYear()
     ].map((el) => el <= 9 ? `0${el}` : `${el}`)
     return time.join(".")
-}
+} // date?.toLocaleDateString("ru")
 
 function Clock() {
 
@@ -29,11 +29,10 @@ function Clock() {
     const [date, setDate] = useState<Date>(new Date(restoreState('hw9-date', (Date.now()))))
     const [show, setShow] = useState<boolean>(false)
 
-    const [ownDate, setOwnDate] = useState<Date | undefined>(undefined)
     const start = () => {
         let res = window.setInterval(() => {
-            setOwnDate(new Date())
-        }, msInSec(1))
+            setDate(new Date())
+        }, msToSec(1))
         // пишут студенты // запустить часы (должно отображаться реальное время, а не +1)
         // сохранить ид таймера (https://learn.javascript.ru/settimeout-setinterval#setinterval)
         setTimerId(res)
@@ -44,7 +43,6 @@ function Clock() {
         // пишут студенты // поставить часы на паузу, обнулить ид таймера (timerId <- undefined)
     }
     const onMouseEnter = (e:React.MouseEvent<HTMLDivElement>) => {
-        console.log(e.target, 'hw9-watch')
         setShow(true)
         // пишут студенты // показать дату если наведена мышка
     }
@@ -52,12 +50,13 @@ function Clock() {
         setShow(false)
     }
 
-    const stringTime = timeCreator(ownDate ?? new Date()) || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
-    const stringDate = dateCreator(ownDate ?? new Date()) || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
+    const stringTime = timeCreator(date) || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
+    const stringMonth = date?.toLocaleDateString("en",{month: "long"}) || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
+
 
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
-    const stringDay = 'date->day' || <br/> // пишут студенты
-    const stringMonth = 'date->month' || <br/> // пишут студенты
+    const stringDay = date?.toLocaleDateString("en",{weekday: "long"}) || <br/> // пишут студенты
+    const stringDate =   dateCreator(date) || <br/> // пишут студенты
 
     return (
         <div className={s.clock}>
@@ -67,7 +66,7 @@ function Clock() {
                 onMouseEnter={onMouseEnter}
                 onMouseLeave={onMouseLeave}
             >
-                <span id={'hw9-day'}>{stringDate}</span>,{' '}
+                <span id={'hw9-day'}>{stringDay}</span>,{' '}
                 <span id={'hw9-time'}>
                     <strong>{stringTime}</strong>
                 </span>
@@ -77,8 +76,8 @@ function Clock() {
                 <div className={s.more}>
                     {show ? (
                         <>
-                            <span id={'hw9-month'}>{stringMonth}</span>,{' '}
-                            <span id={'hw9-date'}>{stringDay}</span>
+                            <span id={'hw9-month'}>{stringDate}</span>,{' '}
+                            <span id={'hw9-date'}>{stringMonth}</span>
                         </>
                     ) : (
                         <>
