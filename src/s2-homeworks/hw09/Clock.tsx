2e74652994eb0b1dad2973c2144e80331036145e
Young Eric <erikbonopart12@gmail.com>
2023-07-10 14:50:46 +0300

    feat: -change data format in time in hw9

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -30,7 +30,7 @@ function Clock() {
     const [show, setShow] = useState<boolean>(false)
 
     const start = () => {
-        let res = window.setInterval(() => {
+        let res:number = +setInterval(() => {
             setDate(new Date())
         }, msToSec(1))
         // пишут студенты // запустить часы (должно отображаться реальное время, а не +1)
@@ -50,13 +50,13 @@ function Clock() {
         setShow(false)
     }
 
-    const stringTime = timeCreator(date) || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
-    const stringMonth = date?.toLocaleDateString("en-US",{month: "long"}) || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
+    const stringTime = date.toLocaleTimeString('ru-RU') || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
+    const stringMonth = new Intl.DateTimeFormat('en-US', {month: 'long'}).format(date) || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
 
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
-    const stringDay = date?.toLocaleDateString("en-US",{weekday: "long"}) || <br/> // пишут студенты
-    const stringDate =   dateCreator(date) || <br/> // пишут студенты
+    const stringDay = new Intl.DateTimeFormat('en-US', {weekday: 'long'}).format(date) || <br/> // пишут студенты
+    const stringDate =  date.toLocaleDateString('ru-RU') || <br/> // пишут студенты
 
     return (
         <div className={s.clock}>
@@ -66,9 +66,9 @@ function Clock() {
                 onMouseEnter={onMouseEnter}
                 onMouseLeave={onMouseLeave}
             >
-                <span id={'hw9-day'}>{stringDay}</span>
+                <span id={'hw9-day'}>{stringDay}</span>,{' '}
                 <span id={'hw9-time'}>
-                    <strong>{timerId ? `, ${stringTime}`  : ""}</strong>
+                    <strong>{stringTime}</strong>
                 </span>
             </div>
 
